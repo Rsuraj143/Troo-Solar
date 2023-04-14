c6ae63c6974d3c4ccc8e9319c76bfc776fa289bc
Rsuraj143 <raudasuraj7@gmail.com>
2023-04-14 18:47:43 +0530

    first commit

---
+++ src/Components/Header/HeaderData.js
@@ -0,0 +1,113 @@
+
+export const HeaderData = [
+  {
+    title: "Home",
+    path: "/Home",
+  },
+  {
+    title: "About_Us",
+    path: "/Home/About",
+  },
+  {
+    title: "Our Project",
+    path: "#",
+    subNav: [
+      {
+        subTitle: "Our Project",
+        heading: " Pick a Course to Get Started ",
+        path: "/Home/Our_Projects",
+      },
+      {
+        subTitle: "Project Details",
+        heading: "Complete Advance Management Study",
+        path: "/Home/Our_Projects/Project_Details",
+      },
+    ],
+  },
+  {
+    title: "Our Service",
+    path: "#",
+    subNav: [
+      {
+        subTitle: "Our Service",
+        heading: " Pick a Course to Get Started ",
+        path: "/Home/Our_Services",
+      },
+      {
+        subTitle: "Our Service List",
+        heading: "Complete Advance Management Study",
+        path: "/Home/Our_Service_List",
+      },
+      {
+        subTitle: "Service Details",
+        heading: "Complete Advance Management Study",
+        path: "/Home/Our_Services/Service_Details",
+      },
+    ],
+  },
+  {
+    title: "Pages",
+    path: "#",
+    subNav: [
+      {
+        subTitle: "Our Team",
+        heading: "",
+        path: "/Home/Our_Team",
+      },
+      {
+        subTitle: "Testimonial",
+        heading: "",
+        path: "/Home/Testimonial",
+      },
+      {
+        subTitle: "FAQs",
+        heading: "",
+        path: "/Home/FAQs",
+      },
+      {
+        subTitle: "Pricing Table",
+        heading: "",
+        path: "/Home/Pricing_Table",
+      },
+      {
+        subTitle: "Comming Soon",
+        heading: "",
+        path: "/Home/Comming_Soon",
+      },
+      {
+        subTitle: "404",
+        heading: "",
+        path: "/Home/404",
+      },
+    ],
+  },
+  {
+    title: "Blogs",
+    path: "#",
+    subNav: [
+      {
+        subTitle: "Blogs",
+        heading: "",
+        path: "/Home/Blogs",
+      },
+      {
+        subTitle: "Blog List",
+        heading: "",
+        path: "/Home/Blogs/Blog_List",
+      },
+      {
+        subTitle: "Blog Details",
+        heading: "",
+        path: "/Home/Blogs/Blog_Details",
+      },
+    ],
+  },
+  {
+    title: "Contact Us",
+    path: "/Home/Contact_Us",
+  },
+  {
+    title: "Get Qoute",
+    path: "/Home/Get_Qoute",
+  },
+];
---
+++ src/Components/Header/NavMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+
+const NavMenu = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default NavMenu
